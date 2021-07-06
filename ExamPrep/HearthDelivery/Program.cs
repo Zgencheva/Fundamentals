7708973541058246f7f712016953a6f4f149f43a
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2021-07-06 21:58:40 +0300

    HearthDelivery

---
+++ ExamPrep/HearthDelivery/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace HearthDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] neighborhood = Console.ReadLine().Split("@").Select(int.Parse).ToArray();
+            int cupidLocation = 0;
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Love!")
+                {
+                    break;
+                }
+                string[] cmd = command.Split().ToArray();
+                int jump = int.Parse(cmd[1]);
+                if (cupidLocation + jump >= neighborhood.Length)
+                {
+                    cupidLocation = 0;
+                    if (neighborhood[cupidLocation] == 0)
+                    {
+                        Console.WriteLine($"Place {cupidLocation} already had Valentine's day.");
+                        continue;
+                    }
+
+                    neighborhood[cupidLocation] -= 2;
+                    if (neighborhood[cupidLocation] == 0)
+                    {
+                        Console.WriteLine($"Place {cupidLocation} has Valentine's day.");
+                        continue;
+                    }
+                    continue;
+                }
+                cupidLocation += jump;
+                if (neighborhood[cupidLocation] == 0)
+                {
+                    Console.WriteLine($"Place {cupidLocation} already had Valentine's day.");
+                    continue;
+                }
+                
+                neighborhood[cupidLocation] -= 2;
+                if (neighborhood[cupidLocation] == 0)
+                {
+                    Console.WriteLine($"Place {cupidLocation} has Valentine's day.");
+                }
+
+            }
+
+            Console.WriteLine($"Cupid's last position was {cupidLocation}.");
+            if (neighborhood.Sum()==0)
+            {
+                Console.WriteLine("Mission was successful.");
+            }
+            else
+            {
+                int failed = neighborhood.Count(x => x != 0);
+                Console.WriteLine($"Cupid has failed {failed} places.");        
+            }
+            
+        }
+    }
+}
---
+++ ExamPrep/ShoppingList/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shoppingList = Console.ReadLine().Split("!").ToList();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Go Shopping!")
+                {
+                    break;
+                }
+
+                string[] cmd = command.Split().ToArray();
+                string status = cmd[0];
+                string grocery = cmd[1];
+                
+                if (status == "Urgent")
+                {
+                    if (shoppingList.Any(x=> x == grocery))
+                    {
+                        continue;
+                    }
+                    List<string> shoppingL2 = new List<string>();
+                    shoppingL2.Add(grocery);
+                    for (int i = 0; i < shoppingList.Count; i++)
+                    {
+                        shoppingL2.Add(shoppingList[i]);
+                    }
+                    shoppingList = shoppingL2;
+                }
+                else if (status == "Unnecessary")
+                {
+                    shoppingList.Remove(grocery);
+                }
+                else if (status == "Correct")
+                {
+                    if (shoppingList.Any(x => x == grocery))
+                    {
+                        string newItem = cmd[2];
+                        int index = shoppingList.IndexOf(grocery);
+                        shoppingList[index] = newItem;
+                    }
+                    
+                }
+                else if (status == "Rearrange")
+                {
+                    if (shoppingList.Any(x=> x == grocery))
+                    {
+                        shoppingList.Remove(grocery);
+                        shoppingList.Add(grocery);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", shoppingList));
+        }
+    }
+}
