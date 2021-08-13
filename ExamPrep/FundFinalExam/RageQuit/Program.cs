f6ed5cedde7bb146dab9e8500964f885cf94812e
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2021-08-13 15:35:07 +0300

    RageQuit and WinningTicket

---
+++ ExamPrep/FundFinalExam/RageQuit/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RageQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([\D]{1,})(\d{1,})";
+            string input = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(input, pattern);
+            StringBuilder sb = new StringBuilder();
+            foreach (Match match in matches)
+            {
+                string word = match.Groups[1].Value.ToString();
+                int timesToRepeat = int.Parse(match.Groups[2].Value.ToString());
+                
+                for (int i = 0; i < timesToRepeat; i++)
+                {
+                    sb.Append(word.ToUpper());
+                }
+            }
+
+            string newWord = sb.ToString();
+            string uniquePattern = @"(?:(.)(?!.*\1))";
+            MatchCollection uniquwMatches = Regex.Matches(newWord, uniquePattern);
+            int num = uniquwMatches.Count;
+
+            Console.WriteLine($"Unique symbols used: {num}");
+            Console.WriteLine(newWord);
+
+
+
+        }
+    }
+}
---
+++ ExamPrep/FundFinalExam/WinningTicket/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace WinningTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = Console.ReadLine();
+            string withoutWS = RemoveWhitespace(input1);
+            string[] input = withoutWS.Split(",", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!IsTicketValid(input[i]))
+                {
+                    Console.WriteLine("invalid ticket");
+                    continue;
+                }
+                string regexPattern = @"([\@\#\$\^]{6,10})(.*)\1";
+                string item = input[i];
+                var match = Regex.Match(item, regexPattern);
+                if (match.Success)
+                {
+                   
+                    var winnings = match.Groups;
+                    var winningGroup = winnings[1].ToString();
+                    char winningChar = winningGroup[0];
+                    if (winningGroup.Length == 20)
+                    {
+                        Console.WriteLine($"ticket \"{input[i]}\" - 10{winningChar} Jackpot!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ticket \"{input[i]}\" - {winningGroup.Length}{winningChar}");
+                    }
+                    
+
+                }
+                else
+                {
+                    Console.WriteLine($"ticket \"{input[i] }\" - no match");
+                }
+            }
+            static bool IsTicketValid(string inputWord)
+            {
+                if (inputWord.Length == 20)
+                {
+                    return true;
+                }
+
+                return false;
+
+            }
+            static string RemoveWhitespace(string input)
+            {
+                return new string(input.ToCharArray()
+                    .Where(c => !Char.IsWhiteSpace(c))
+                    .ToArray());
+            }
+        }
+
+        
+
+    }
+}
